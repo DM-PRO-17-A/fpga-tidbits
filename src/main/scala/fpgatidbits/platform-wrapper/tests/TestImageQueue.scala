b125a270aeb1ea46b139cb411864c372872b9b2b
Pontius1007 <embrikeinang@msn.com>
2017-11-11 18:00:42 +0100

    Added new module for reading from Queue to CPU

--- src/main/scala/fpgatidbits/platform-wrapper/tests/TestImageQueue.scala
+++ src/main/scala/fpgatidbits/platform-wrapper/tests/TestImageQueue.scala
@@ -18,28 +18,27 @@ class TestImageQueue (p: PlatformWrapperParams) extends GenericAccelerator(p) {
         val full = Bool(OUTPUT)
     }
 
-    val queue = Module(new ImageQueue(data_width, queue_depth, vec_size, true)).io
+    val queue = Module(new ImageQueue(data_width, queue_depth, vec_size)).io
 
     queue.input_data <> io.input_data
-    queue.pulse <> io.input_pulse
+    queue.input_pulse <> io.input_pulse
     queue.full <> io.full
 
     val pulse_reg = Reg(next=io.input_pulse)
-    val counter = Reg(init=UInt(width=8))
+    val counter = Reg(init=UInt(width=10))
     when(!io.input_pulse && pulse_reg){
         counter := counter + UInt(1)
-        printf("counter %d\n", counter)
+        //printf("counter %d\n", counter)
     }
-    
-    println("kdjfskdfhskdfhskdjfskdjfhskdjhfskjdhf")
-    println(queue_depth)
-    when(counter >= UInt(62)){
+
+    //println(queue_depth)
+    when(counter >= UInt(384)){
         queue.output_data.ready := Bool(true)
         when(queue.output_data.valid){
-            printf("Output: %d\n", queue.output_data.bits(0))
+            //printf("Output: %d\n", queue.output_data.bits(0))
         }
     }.otherwise{
         queue.output_data.ready := Bool(false)
     }
-    printf("V: %b R: %b D: %d S: %d\n", queue.output_data.valid, queue.output_data.ready, queue.output_data.bits(0), queue.count)
+    printf("V: %b R: %b D: %d \n", queue.output_data.valid, queue.output_data.ready, queue.output_data.bits(0))
 }
--- src/main/scala/fpgatidbits/rosetta/OutputQueue.scala
+++ src/main/scala/fpgatidbits/rosetta/OutputQueue.scala
@@ -4,27 +4,34 @@ import Chisel._
 import fpgatidbits.ocm._
 
 
-class ImageQueue(dataWidth: Int, queueDepth: Int, vec_fill_size: Int) extends Module {
+class OutputQueue(dataWidth: Int, queueDepth: Int, vec_fill_size: Int) extends Module {
     val io = new Bundle {
-        val input_data = Vec.fill(vec_fill_size){UInt(INPUT, width=dataWidth)}
-        val input_pulse = Bool(INPUT)
+        val input_data = Flipped(Decoupled(Vec.fill(vec_fill_size){UInt(INPUT, width=dataWidth)}))
 
-        val output_data = Decoupled(Vec.fill(vec_fill_size){UInt(OUTPUT, dataWidth)})
+        val output_data = Vec.fill(vec_fill_size){UInt(OUTPUT, dataWidth)}
+        val output_pulse = Bool(INPUT)
+
         val full = Bool(OUTPUT)
         val empty = Bool(OUTPUT)
 
     }
 
-    val pulse_reg = Reg(next=io.input_pulse)
+    val pulse_reg = Reg(next=io.output_pulse)
     val queue = Module(new FPGAQueue(Vec.fill(vec_fill_size){UInt(width=dataWidth)}, queueDepth))
+    val output_ready = (!io.output_pulse && pulse_reg)
+    val output_reg = Reg(init=Vec.fill(vec_fill_size){UInt(width=dataWidth)})
 
-    queue.io.enq.valid := !io.input_pulse && pulse_reg
-    queue.io.enq.bits := io.input_data
-    
-    io.full := (queue.io.count === UInt(queueDepth -1))
-    io.empty := (queue.io.count === UInt(0))
+    queue.io.enq <> io.input_data
 
-    io.output_data <> queue.io.deq
+    queue.io.deq.ready := output_ready
+
+    when(output_ready){
+        io.output_data := queue.io.deq.bits
+        output_reg := queue.io.deq.bits
+    } .otherwise {
+        io.output_data := output_reg
+    }
+
 
 
     /*
@@ -32,11 +39,10 @@ class ImageQueue(dataWidth: Int, queueDepth: Int, vec_fill_size: Int) extends Mo
         printf("ADD: (%d, %d, %d, %d, %d, %d, %d, %d), len: %d\n", queue.io.enq.bits(0), queue.io.enq.bits(1), queue.io.enq.bits(2), queue.io.enq.bits(3), queue.io.enq.bits(4),
             queue.io.enq.bits(5), queue.io.enq.bits(6), queue.io.enq.bits(7), queue.io.count)
     }
-    // Printsetning som printer alle bits i en dequeue-operasjon når valid og ready er true.
+     Printsetning som printer alle bits i en dequeue-operasjon når valid og ready er true.
     when(queue.io.deq.valid && queue.io.deq.ready){
         printf("POP: (%d, %d, %d, %d, %d, %d, %d, %d), len: %d\n", queue.io.deq.bits(0), queue.io.deq.bits(1), queue.io.deq.bits(2), queue.io.deq.bits(3), queue.io.deq.bits(4),
             queue.io.deq.bits(5), queue.io.deq.bits(6), queue.io.deq.bits(7), queue.io.count)
     }
     */
-
 }
